d4c99afb054f8a9c9b8b81a0015de20fb833f148
Martynas L <martynas@martynass-mac.local>
2018-04-18 06:00:33 -0700

    Initial Commit

---
+++ MCGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MCGame
+//
+//  Created by Martynas L on 18/04/2018.
+//  Copyright © 2018 Martynas L. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
